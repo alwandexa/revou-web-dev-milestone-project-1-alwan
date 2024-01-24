e090d57465204370bd9bcce6b73ef7841f8eb059
alwandexa <alwan.wirawan@djembatandua.com>
2024-01-24 09:43:14 +0700

    fix custom validation message for password

--- login.js
+++ login.js
@@ -1,8 +1,10 @@
 const passwordInvalid = () => {
+  const password = document.getElementById("password");
   const message =
     "Minimum eight characters, at least one uppercase letter, one lowercase letter, one number and one special character !";
 
-  document.getElementById("password").setCustomValidity(message);
+  password.oninput = password.setCustomValidity("");
+  password.oninvalid = password.setCustomValidity(message);
 };
 
 passwordInvalid();
